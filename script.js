112055644eb46165f44d67aeade1aaef5b244274
thibaud.guadagna <thibaud.guadagna@gmail.com>
2025-03-21 16:25:19 +0100

    ajout du menu burger

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
 const button = document.getElementById('burgerBtn');
 button.addEventListener('click', () => {
     const buttons = document.getElementById("openMenu")
-  buttons.classList.toggle('hidden')
+  buttons.classList.toggle('translate-x-full')
 })
-=======
-const sideNav = document.getElementById("burgerBtn");
-
-sideNav.onclick = openNav;
-sideNav.onclick = closeNav;
-
-function openNav() {
-    sideNav.classList.add("active");
-    let isOpen = true
-    console.log(isOpen)
-}
-
->>>>>>> 35c090863a028a8c678e5d6ca68857494ab4ef4b
