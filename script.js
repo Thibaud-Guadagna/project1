8b761ffbb6f3449ab43986efc8c76f2b0ed77f2a
thibaud.guadagna <thibaud.guadagna@gmail.com>
2025-03-24 16:48:55 +0100

    pushforyavuz

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const button = document.getElementById('burgerBtn');
+const button1 = document.getElementById('burgerBtn1');
 const bar1 = document.getElementById('barre1');
 const bar2 = document.getElementById('barre2');
 const bar3 = document.getElementById('barre3');
+const menu = document.getElementById("openMenu")
+
 button.addEventListener('click', () => {
-  const buttons = document.getElementById("openMenu")
-  buttons.classList.toggle('translate-x-full')
+  button.classList.add('z-999999')
+  menu.classList.toggle('translate-x-full')
+  console.log('click')
+
+
+  bar1.classList.toggle('rotate-45')
+  bar1.classList.toggle('translate-y-2.5')
+
+  bar2.classList.toggle('opacity-0')
+
+  bar3.classList.toggle('-rotate-45')
+  bar3.classList.toggle('-translate-y-2.5')
+})
+button1.addEventListener('click', () => {
+  button.classList.add('z-999999')
+  menu.classList.toggle('translate-x-full')
+  console.log('click')
+
 
   bar1.classList.toggle('rotate-45')
   bar1.classList.toggle('translate-y-2.5')
