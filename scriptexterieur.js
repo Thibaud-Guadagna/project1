70e93392a01563da3fbb2023d04b5899c8bce86c
Alexandra PASTOR <pastoralex@free.fr>
2025-03-26 22:06:36 +0100

    add btn scroll

--- scriptexterieur.js
+++ scriptexterieur.js
@@ -1,3 +1,20 @@
+// Panier //
+document.addEventListener("DOMContentLoaded", function () {
+	const openbtn = document.getElementById("pictopanier");
+	const closebtn = document.getElementById("closebtn");
+	const sideBar = document.getElementById("panier");
+
+	openbtn.addEventListener("click", () => {
+		console.log("Ouverture du panier");
+		sideBar.classList.toggle("translate-x-full");
+	});
+
+	closebtn.addEventListener("click", () => {
+		console.log("Fermeture du panier");
+		sideBar.classList.add("translate-x-full");
+	});
+});
+
 document.addEventListener("DOMContentLoaded", () => {
 	// Menu Burger //
 	const button = document.getElementById("burgerBtn");
@@ -32,24 +49,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 		bar3.classList.toggle("-rotate-45");
 		bar3.classList.toggle("-translate-y-2.5");
-	});
-
-	// Panier //
-
-	document.addEventListener("DOMContentLoaded", function () {
-		const openbtn = document.getElementById("pictopanier");
-		const closebtn = document.getElementById("closebtn");
-		const sideBar = document.getElementById("panier");
-
-		openbtn.addEventListener("click", () => {
-			console.log("Ouverture du panier");
-			sideBar.classList.toggle("translate-x-full");
-		});
-
-		closebtn.addEventListener("click", () => {
-			console.log("Fermeture du panier");
-			sideBar.classList.add("translate-x-full");
-		});
 	});
 
 	// Bouton Commander //
